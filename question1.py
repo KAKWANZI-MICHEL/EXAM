d2ff6fd9072f2d4a41ac48b8bc60d9d826995e0a
kakwanzi michel <michelkakwanzi@gmail.com>
2024-10-10 10:41:33 +0300

    EXAM SOLUTIONS

---
+++ question1.py
@@ -0,0 +1,7 @@
+# using a function ,calculate a program that calculate the volume of cylinder
+import math
+height=int(input("Enter the height of the cylinder: "))
+radius=int(input("Enter the radius of the cylinder: "))
+pie=math.pi
+volume=pie *radius**2 * height
+print(volume)
---
+++ question2.py
@@ -0,0 +1,5 @@
+# create a program that calculates the area of a triangle
+base= int(input("Enter the base of a triangle: "))
+height=int(input("Enter the height of the triangle: "))
+area= (1/2)*base*height
+print(f"The area of the triangle is: {area} cm")
